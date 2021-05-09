5ff74954fa9f81649fd1f96d6c1789d9fa430db0
nasheshin <nasheshin01@gmail.com>
2021-05-09 14:29:35 +0500

    Сделана задача 18

---
+++ Task18/Program.cs
@@ -0,0 +1,60 @@
+﻿#nullable enable
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace Task18
+{
+    internal static class Program
+    {
+        private static IEnumerable<TSource> DistinctBy<TSource, TKey>(
+            this IEnumerable<TSource> source,
+            Func<TSource, TKey> keySelector)
+        {
+            var knownKeys = new HashSet<TKey>();
+            return source.Where(element => knownKeys.Add(keySelector(element)));
+        }
+        
+        private static IEnumerable<Tuple<string, string>> BypassDoc(XElement element)
+        {
+            var attrs = element.Attributes().Select(attr => new Tuple<string, string>(attr.Name.ToString(), attr.Value)).ToList();
+            foreach(var newElement in element.Elements())
+                attrs.AddRange(BypassDoc(newElement));
+            return attrs;
+        }
+
+        private static void Execute(string inputPath)
+        {
+            var document = XDocument.Load(inputPath);
+
+            if (document.Root == null)
+            {
+                return;
+            }
+
+            var result = BypassDoc(document.Root).DistinctBy(pair => pair.Item1).ToList();
+            var attrNames = result.Select(item => item.Item1).OrderBy(i => i).Distinct().ToList();
+            var attrValues = result.Select(item => item.Item2).OrderBy(i => i).ToList();
+
+            foreach (var attrName in attrNames)
+            {
+                Console.Write(attrName + " ");
+            }
+
+            Console.WriteLine();
+            foreach (var attrName in attrValues)
+            {
+                Console.Write(attrName + " ");
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            const string? path = @"D:\HSE\Программирование\Lab6\LINQToXML\Task18\Files";
+
+            Execute(Path.Combine(path, "Input.xml"));
+        }
+    }
+}
---
+++ Task8/Program.cs
@@ -0,0 +1,59 @@
+﻿#nullable enable
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Task8
+{
+    internal static class Program
+    {
+        private static void Execute(string filePath, string toCreatePath)
+        {
+            var reader = new StreamReader(filePath);
+            
+            var lines = new List<string>();
+            string? line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                lines.Add(line);
+            }
+
+            var wordsByLines = lines.Select(curLine => new List<string>(curLine.Split(' '))).ToList();
+            foreach (var curLine in wordsByLines)
+            {
+                curLine.Sort();
+            }
+
+            var documentXml = new XDocument(
+                new XDeclaration("1.0", "utf-8", null),
+                new XElement("root"));
+            
+            foreach (var curLine in wordsByLines)
+            {
+                var track = new XElement("line");
+                foreach (var word in curLine)
+                {
+                    var trackWord = new XElement("word");
+                    foreach (var character in word)
+                    {
+                        trackWord.Add(new XElement("char", character));
+                    }
+                    track.Add(trackWord);
+                }
+                documentXml.Root?.Add(track);
+            }
+            
+            documentXml.Save(toCreatePath);
+        }
+        
+        private static void Main(string[] args)
+        {
+            const string? pathTask8 = @"D:\HSE\Программирование\Lab6\LINQToXML\Task8\FilesTask8";
+
+            Execute(Path.Combine(pathTask8, "File.txt"), Path.Combine(pathTask8, "Output.xml"));
+        }
+    }
+}
